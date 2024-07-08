1c48b7dd1a852b3c0416c2795e5ab2d74b197427
Abhishek Kumar <abhik12295@gmail.com>
2024-07-08 13:58:19 -0500

    Made changes

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import streamlit as st
 from PIL import Image
 import requests
 from streamlit_lottie import st_lottie
-import altair
 
 # --Path Setting--
 current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
@@ -21,7 +20,7 @@ SOCIAL_MEDIA = {
 }
 
 st.set_page_config(
-    page_title="Abhishek Kr🧊",
+    page_title="Abhishek Kumar Portfolio",
     page_icon=":computer:",
     layout="wide",
 )
@@ -39,14 +38,11 @@ def local_css(file_name):
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 
-#  To get rid of the Streamlit branding stuff
-local_css("styles/main.css")
-
-#  Anchor
-st.title("#")  # This anchor is needed for the page to start at the top when it is called.
+# Load CSS
+local_css(css_file)
 
 
-# background
+# Background
 def add_bg_from_url():
     st.markdown(
         f"""
@@ -70,146 +66,130 @@ add_bg_from_url()
 with open(resume_file, "rb") as pdf_file:
     PDF_Byte = pdf_file.read()
 pro_pic = Image.open(profile_pic)
+
 with st.container():
     col1, col2 = st.columns((2, 2))
 
     with col1:
         st.image(pro_pic, width=200)
-
     with col2:
-        pass
-        # st.write("Click Below to download the file :point_down: ")
-        # st.download_button(
-        #     label="Download Resume :page_facing_up:",
-        #     data=PDF_Byte,
-        #     file_name=resume_file.name,
-        #     mime="application/octet-stream", )
-        # st.write(":email:", EMAIL)
-
-
-with st.container():
-    col1, col2 = st.columns((2, 1))
-    with col1:
-        st.title("Welcome to my Portfolio Page!")
-        st.subheader("Hi, I am Abhishek :desktop_computer:")
-        st.subheader(
-            """
-            I am a *Software Developer* who specializes in the broad fields of *Automation*, *Tech* & *Data*.
-            """
+        st.title("Abhishek Kumar")
+        st.subheader("Software Developer | Automation | Data Enthusiast")
+        st.download_button(
+            label="📄 Download Resume",
+            data=PDF_Byte,
+            file_name=resume_file.name,
+            mime="application/octet-stream"
         )
-        st.write("""""")
-        st.subheader(
-            """
-            Love for 👩‍💻 :cricket_bat_and_ball: and :soccer:
-            Technical leader, devise effective solutions to complex business challenges with an innate ability for 
-            teamwork, task prioritization, big picture vision, and dedication. 
-            Masterfully code key projects to enable on-time delivery of key milestones within budget constraints.
-            """
-        )
-    with col2:
-        st_lottie(
-            load_lottieurl("https://assets10.lottiefiles.com/private_files/lf30_wqypnpu5.json"),
-            height=300,
-        )
+        st.write(f"📧 {EMAIL}")
 
-# --- Education ---
+# --- ABOUT ME ---
 with st.container():
     st.write("---")
+    st.header("About Me")
+    st.write(
+        """
+        Hi, I am Abhishek, a Software Developer specializing in Automation, Tech, and Data. 
+        Passionate about solving complex problems and delivering efficient solutions.
+        """
+    )
+    st_lottie(
+        load_lottieurl("https://assets10.lottiefiles.com/private_files/lf30_wqypnpu5.json"),
+        height=300,
+    )
+
+# --- EDUCATION ---
+with st.container():
+    st.write("---")
+    st.header("Education")
     col1, col2 = st.columns(2)
+
     with col1:
-        st_lottie(
-            load_lottieurl("https://assets5.lottiefiles.com/packages/lf20_uqsv3ztj.json"),
-            height=300,
-        )
-    with col2:
-        st.header("Education")
-        st.subheader(
+        st.subheader("Illinois Institute of Technology")
+        st.write(
             """
-             	:male-student: Illinois Institute of Technology \n
-             	:round_pushpin: Chicago, Illinois, US \n 
-             	:calendar: Jan 2021 - Dec 2022 \n
-             	🧑‍💻 Master's in Computer Science (with specialialization in Data Analytics) \n
-            """
-        )
-        st.subheader(
-            """
-                :male-student: University of Petroleum and Energy Studies \n
-                :round_pushpin: Dehradun, Uttarakhand, India \n
-                :calendar: June 2013 - Jun 2017 \n
-                🧑‍💻 Bachelor's in Computer Science and Engineering \n
+            📍 Chicago, Illinois, US
+            📅 Jan 2021 - Dec 2022
+            🧑‍💻 Master's in Computer Science (Data Analytics)
             """
         )
 
-# --- Work Experience---
+    with col2:
+        st.subheader("University of Petroleum and Energy Studies")
+        st.write(
+            """
+            📍 Dehradun, Uttarakhand, India
+            📅 June 2013 - Jun 2017
+            🧑‍💻 Bachelor's in Computer Science and Engineering
+            """
+        )
+
+# --- WORK EXPERIENCE ---
 with st.container():
     st.write("---")
+    st.header("Work Experience")
     col1, col2 = st.columns(2)
-    with col2:
-        st_lottie(
-            load_lottieurl("https://assets7.lottiefiles.com/packages/lf20_w51pcehl.json"),
-            height=300,
-        )
+
     with col1:
-        st.header("Work Experience")
-        st.subheader(
+        st.subheader("Kuehne & Nagel Inc.")
+        st.write(
             """
-                💼  Kuehne & Nagel Inc. \n
-                :bookmark_tabs: BI & Data Science Developer \n 
-                :calendar: Apr 2022 - Now \n
-                :pushpin: | Data Engineering | ETL | Python | AWS Glue | Tableau | AWS |
-                 Built ETL pipelines, optimized processing time by 50% for automated report generation \n
-            """
-        )
-        st.subheader(
-            """
-                💼  CCC Intelligent Solutions \n
-                :bookmark_tabs: Automation Analyst Intern \n 
-                :calendar: Jan 2022 - Dec 2022 \n
-                :pushpin: | Automation | Data Analytics | Python | MongoDB |
-                 Built Audit Dashboard which reduced time to prepare reports by 40% \n
+            📅 Apr 2023 - Present
+            🏷️ BI & Data Science Developer
+            - Data Engineering | ETL | Python | AWS Glue | Tableau | AWS
+            - Built ETL pipelines, optimized processing time by 50% for automated report generation
             """
         )
-        st.subheader(
+
+        st.subheader("CCC Intelligent Solutions")
+        st.write(
             """
-                 💼 Must Discover Private Limited \n
-                :bookmark_tabs: Software Engineer\n
-                :calendar: Jul 2017 - Apr 2020 \n
-                :pushpin: | Software Engineer skills | Web Development | Development | Codes | AWS | Data Science |
-                Achieved 60% accuracy in targeted marketing campaigns.  \n
+            📅 Jan 2022 - Dec 2022
+            🏷️ Automation Analyst Intern
+            - Automation | Data Analytics | Python | MongoDB
+            - Built Audit Dashboard which reduced time to prepare reports by 40%
             """
         )
-        st.subheader(
+
+    with col2:
+        st.subheader("Must Discover Private Limited")
+        st.write(
             """
-                 💼 Nuriss LifeCare Private Limited \n
-                :bookmark_tabs: Android Developer Intern \n
-                :calendar: May 2016 - Jul 2016 \n
-                :pushpin: | Android Development | Java | RESTful API | Firebase | E-commerce |
-                Application for medical representatives and pharmacies with customized medicine inventory and orders.\n
+            📅 Jul 2017 - Apr 2020
+            🏷️ Software Engineer
+            - Software Engineer skills | Web Development | Development | Codes | AWS | Data Science
+            - Achieved 60% accuracy in targeted marketing campaigns.
+            """
+        )
+
+        st.subheader("Nuriss LifeCare Private Limited")
+        st.write(
+            """
+            📅 May 2016 - Jul 2016
+            🏷️ Android Developer Intern
+            - Android Development | Java | RESTful API | Firebase | E-commerce
+            - Developed an application for medical representatives and pharmacies.
             """
         )
 
 # --- TECH STACK / SKILLS ---
 with st.container():
     st.write("---")
+    st.header("Tech Stack / Skills")
     col1, col2 = st.columns(2)
-    with col2:
-        st.header("Tech Stack / Skills")
-        st.write(
-            """
-            Languages
-            - Python, Spark, Javascript, Java
-            Frameworks
-            - PySpark, FastAPI, Django, Flask, Bootstrap, Node.js
-            Databases
-            - MySQL, PostgreSQL, MongoDB
-            Hosting & Cloud
-            - AWS, Azure, Heroku, Streamlit Cloud
-            Miscellaneous
-            - Git, Github, Gitlab, CI/CD, Docker, ML/AI, Tableau
-             """
-        )
 
     with col1:
+        st.write(
+            """
+            **Languages:** Python, Spark, Javascript, Java
+            **Frameworks:** PySpark, FastAPI, Django, Flask, Bootstrap, Node.js
+            **Databases:** MySQL, PostgreSQL, MongoDB
+            **Hosting & Cloud:** AWS, Azure, Heroku, Streamlit Cloud
+            **Miscellaneous:** Git, Github, Gitlab, CI/CD, Docker, ML/AI, Tableau
+            """
+        )
+
+    with col2:
         st_lottie(
             load_lottieurl("https://assets4.lottiefiles.com/packages/lf20_lln7m43m.json"),
             height=300,
@@ -219,158 +199,93 @@ with st.container():
 with st.container():
     st.write("---")
     st.header("Portfolio")
-    st.subheader("Take a look at some of my works. !")
-
+    st.subheader("Take a look at some of my works!")
     col1, col2, col3 = st.columns(3)
-    with col1:
-        st.image(
-            "https://images.pexels.com/photos/21014/pexels-photo.jpg")
-        st.subheader("Map Route Algorithm")
-        st.write("Built a route-planning algorithm like the one used in Google Maps to calculate the shortest path "
-                 "between two points on a map.")
 
+    with col1:
+        st.image("https://images.pexels.com/photos/21014/pexels-photo.jpg")
+        st.subheader("Map Route Algorithm")
+        st.write(
+            "Built a route-planning algorithm like the one used in Google Maps to calculate the shortest path between two points on a map.")
         if st.button('Github', key="ews_github"):
-            st.write('Github opens in new browser tab')
-            #js = "window.open('https://github.com/abhik12295/Map-Router-Planner')"  # New tab or window
-            # html = '<img src onerror="{}">'.format(js)
-            # div = Div(text=html)
-            # st.bokeh_chart(div)
             link = '[Click here](https://github.com/abhik12295/Map-Router-Planner)'
             st.markdown(link, unsafe_allow_html=True)
+
     with col2:
-        st.image(
-            "https://images.pexels.com/photos/1046398/pexels-photo-1046398.jpeg")
+        st.image("https://images.pexels.com/photos/1046398/pexels-photo-1046398.jpeg")
         st.subheader("Civil Advocacy")
-        st.write("Android Application and Google Civic Information API will be used to acquire the government "
-                 "official data (via REST service and JSON results).")
+        st.write("Android Application using Google Civic Information API to acquire government official data.")
         if st.button('Github', key="gee_github"):
-            st.write('Github opens in new browser tab')
             link = '[Click here](https://github.com/abhik12295/Civil-Advocacy)'
             st.markdown(link, unsafe_allow_html=True)
+
     with col3:
-        st.image(
-            "https://images.pexels.com/photos/4551158/pexels-photo-4551158.jpeg")
+        st.image("https://images.pexels.com/photos/4551158/pexels-photo-4551158.jpeg")
         st.subheader("Illinois Open Cafeteria")
-        st.write("Created a Machine Learning model which suggests a location to open a Cafeteria in Illinois")
-        st.write("""""")
+        st.write("Created a Machine Learning model to suggest a location to open a cafeteria in Illinois.")
         if st.button('Github', key="ccw_github"):
-            st.write('Github opens in new browser tab')
             link = '[Click here](https://github.com/abhik12295/illinois_open_cafetaria)'
             st.markdown(link, unsafe_allow_html=True)
 
+# Additional Portfolio Items
 with st.container():
-    col4, col5, col6 = st.columns(3)
-    with col4:
-        st.image(
-            "https://images.pexels.com/photos/3936358/pexels-photo-3936358.jpeg")
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        st.image("https://images.pexels.com/photos/3936358/pexels-photo-3936358.jpeg")
         st.subheader("COVID Text Analysis Classification")
-        st.write("Worked on Gaussian Naïve Bayes, Random Forest, and TF-IDF Vectorizer which affected the performance "
-                 "of the models")
+        st.write("Worked on Gaussian Naïve Bayes, Random Forest, and TF-IDF Vectorizer to improve model performance.")
         if st.button('Github', key="qrc_github"):
-            st.write('Github opens in new browser tab')
             link = '[Click here](https://github.com/abhik12295/COVID_TextAnalysis_Classification)'
             st.markdown(link, unsafe_allow_html=True)
-    with col5:
-        st.image(
-            "https://images.pexels.com/photos/2335048/pexels-photo-2335048.jpeg")
+
+    with col2:
+        st.image("https://images.pexels.com/photos/2335048/pexels-photo-2335048.jpeg")
         st.subheader("Movie Recommendation Content-Based Filtering")
-        st.write("Content-based (cognitive filtering) recommendation system is to suggest an item based on a "
-                 "comparison of the item's content and a user profile")
-        # if st.button('Enter App', key="ccw_enter"):
-        #     link = '[Click here](https://movie-igniter.herokuapp.com/)'
-        #     st.markdown(link, unsafe_allow_html=True)
+        st.write("Developed a content-based recommendation system to suggest items based on user preferences.")
         if st.button('Github', key="spw_github"):
-            st.write('Github opens in new browser tab')
             link = '[Click here](https://github.com/abhik12295/movie-recommend)'
             st.markdown(link, unsafe_allow_html=True)
-    with col6:
-        st.image(
-            "https://images.pexels.com/photos/1102915/pexels-photo-1102915.jpeg")
+
+    with col3:
+        st.image("https://images.pexels.com/photos/1102915/pexels-photo-1102915.jpeg")
         st.subheader("Weather Application")
-        st.write("Android Application in Java using Visual Crossing Web API")
-        st.write("""""")
-        if st.button('Github', key="bpw_github"):
-            st.write('Github opens in new browser tab')
-            link = '[Click here](https://github.com/abhik12295/WeatherApp)'
+        st.write("Developed an Android application in Java using Visual Crossing Web API.")
+        if st.button('Github', key="xrf_github"):
+            link = '[Click here](https://github.com/abhik12295/weather_application)'
             st.markdown(link, unsafe_allow_html=True)
 
-# SOCIAL network
+# --- SOCIAL LINKS ---
 with st.container():
     st.write("---")
+    st.header("Social Media Links")
     col1, col2, col3, col4 = st.columns(4)
-    with col3:
-        st.image(
-            "https://www.freeiconspng.com/uploads/facebook-logo-new-8.png", width=100)
-    with col2:
-        st.image(
-            "https://www.freeiconspng.com/uploads/git-github-hub-icon-25.png", width=100)
+
     with col1:
-        st.image(
-            "https://www.freeiconspng.com/uploads/linkedin-linkedin-icon-flat-icon-linkedin-png-social-icon-png-11.png",
-            width=100)
+        st.markdown(f"[LinkedIn]({SOCIAL_MEDIA['LinkedIn']})")
+
+    with col2:
+        st.markdown(f"[GitHub]({SOCIAL_MEDIA['GitHub']})")
+
+    with col3:
+        st.markdown(f"[Facebook]({SOCIAL_MEDIA['Facebook']})")
+
     with col4:
-        st.image(
-            "https://www.freeiconspng.com/uploads/twitter-icon-9.png", width=100)
-
-cols = st.columns(len(SOCIAL_MEDIA))
-for index, (platform, link) in enumerate(SOCIAL_MEDIA.items()):
-    cols[index].write(f"[{platform}]({link})")
-
-# Contact me details
-with st.container():
-    st.write("---")
-    st.markdown("<h2 style='text-align: center;'>CONTACT ME</h2>", unsafe_allow_html=True)
-    st.markdown("<h2 style='text-align: center;'>I'd Love To Hear From You</h2>", unsafe_allow_html=True)
-
-    col1, col2, col3 = st.columns(3)
-    with col1:
-        st.image(
-            "https://www.freeiconspng.com/uploads/red-location-map-pin-icon-5.png", width=100)
-        st.subheader("Where To Find Me!")
-        st.write("Chicago, IL")
-    with col2:
-        st.image(
-            "https://www.freeiconspng.com/uploads/-email-letter-mail-send-sign-icon--iconfinder-20.png", width=100)
-        st.subheader("Email Me At")
-        st.write("abhik12295@gmail.com")
-
-    with col3:
-        st.image(
-            "https://www.freeiconspng.com/uploads/mobile-phone-icon-from-colorful-stickers-part-4-set--256x256-px-13"
-            ".png", width=100)
-        st.subheader("Call Me At")
-        st.write("Phone: (+1) 312 468 5096")
+        st.markdown(f"[Twitter]({SOCIAL_MEDIA['Twitter']})")
 
 # --- CONTACT ---
 with st.container():
     st.write("---")
-    st.markdown("<h2 style='text-align: center;'>Feedback</h2>", unsafe_allow_html=True)
-    st.write("##")
-
-    col1, col2, col3 = st.columns(3)
-    with col2:
-        contact_form = """
-        <form action="https://formsubmit.co/059cd784e788c85ed5add9aed225bb53" method="POST">
-            <input type="hidden" name="_captcha" value="true">
-            <input type="text" name="name" placeholder="Your name" required = True>
-            <input type="email" name="email" placeholder="Your email" required>
-            <textarea name="message" placeholder="Your message" required></textarea>
-            <button type="submit">Send</button>
-        </form>
-        """
-        st.markdown(contact_form, unsafe_allow_html=True)
-
-with st.container():
-    for i in range(8):
-        st.write("##")
-    st.write("---")
-    col1, col2 = st.columns(2)
-    with col1:
-        st.write(
-            """
-            ABHISHEK KUMAR :copyright: 2024
-            """
-        )
-    with col2:
-        st.markdown("<p style='text-align: right;'>Made in 2022 with ❤, 🐍 and Streamlit</p>", unsafe_allow_html=True)
-    st.write("---")
+    st.header("Get In Touch With Me!")
+    st.write(
+        "I'd love to hear from you. Whether you have a question or just want to say hi, feel free to drop me a message!")
+    contact_form = """
+    <form action="https://formsubmit.co/059cd784e788c85ed5add9aed225bb53" method="POST">
+        <input type="hidden" name="_captcha" value="false">
+        <input type="text" name="name" placeholder="Your name" required>
+        <input type="email" name="email" placeholder="Your email" required>
+        <textarea name="message" placeholder="Your message here" required></textarea>
+        <button type="submit">Send</button>
+    </form>
+    """
+    st.markdown(contact_form, unsafe_allow_html=True)
